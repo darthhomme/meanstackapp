111943bfdfe75d20956033286815f68bd41f1790
Jeong Kim <joshkim83@outlook.com>
2016-02-09 18:16:46 -0500

    fixed the date problem

--- greylady/public/js/controllers/snippet.js
+++ greylady/public/js/controllers/snippet.js
@@ -1,3 +1,11 @@
+Date.prototype.dateToYMD = function() {
+    var d = this.getDate();
+    var m = this.getMonth() + 1;
+    var y = this.getFullYear();
+    return  y + (m<=9 ? '0' + m : m) +  (d <= 9 ? '0' + d : d);
+}
+
+
 var ctrl = angular.module('snippetsController', ['snippetsApiFactory']);
 ctrl.controller('snippetsController', ['$scope', 'snippetsApi', '$http', '$window', function($scope, snippetsApi, $http, $window){
 
@@ -9,12 +17,7 @@ ctrl.controller('snippetsController', ['$scope', 'snippetsApi', '$http', '$windo
   };
 
   $scope.docs = [];
-  function Date.Prototype.dateToYMD(date) {
-      var d = date.getDate();
-      var m = date.getMonth() + 1;
-      var y = date.getFullYear();
-      return  y + (m<=9 ? '0' + m : m) +  (d <= 9 ? '0' + d : d);
-  }
+
 $scope.getNews = function() {
 
     var fromDate = $scope.dateFrom.dateToYMD();
