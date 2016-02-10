93ac2d8ad460267ae5ee83367a5dfae44a134ae2
Jeong Kim <joshkim83@outlook.com>
2016-02-09 19:00:50 -0500

    eureka

--- greylady/public/js/services/snippetsApi.js
+++ greylady/public/js/services/snippetsApi.js
@@ -3,17 +3,17 @@ api.factory('snippetsApi', ['$http', function($http){
 
   console.log("hello");
 
-  // 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch&begin_date=20160105&end_date=20160112&sort=newest&hl=true&api-key=62bd71e38ae6689ade861f7d1976e48f%3A6%3A74251473'
+  // 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch&begin_date=20160201&end_date=20160202&hl=true&api-key=62bd71e38ae6689ade861f7d1976e48f%3A6%3A74251473'
 
-  var baseUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?sort=newest&hl=true&api-key=';
+  var baseUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch';
   var myKey = '62bd71e38ae6689ade861f7d1976e48f:6:74251473'
-  var superKey = baseUrl + myKey;
 
 
   var snippetsInterface = [];
 
   snippetsInterface.getAllSnippets = function( fromDate, toDate ) {
-    return $http.jsonp( superKey + '&begin_date=' + fromDate + '&end_date=' + toDate + '&callback=JSON_CALLBACK' );
+    return $http.jsonp( baseUrl + '&begin_date=' + fromDate + '&end_date=' + toDate + '&hl=true&api-key=' + myKey);
+    var superKey = baseUrl + '&begin_date=' + fromDate + '&end_date=' + toDate + '&hl=true&api-key=' + myKey;
   };
 
   snippetsInterface.showSnippet = function() {
